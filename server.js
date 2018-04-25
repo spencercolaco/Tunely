510d10a44b35ec853e7fd16cc034b583b352f92c
spencercolaco <spencercola@gmail.com>
2018-04-25 12:40:19 -0500

    removes flipbook

--- server.js
+++ server.js
@@ -90,6 +90,6 @@ app.get('/', function homepage (req, res) {
  **********/
 
 // listen on port 3000
-app.listen(process.env.PORT || 3000, () =>{
+app.listen(process.env.PORT || 3001, () =>{
   console.log("w00t");
 });
